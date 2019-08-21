a25395c0854b40269c07b95269ee572c6051ea76
alucarth <ltorrezs2008@gmail.com>
2019-08-21 09:47:26 -0400

    instalando wkhtml hc4

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>{{ $title }}</title>
-    <link rel="stylesheet" href="{{ public_path('css/materialicons.css') }}" media="all" />
+    {{-- <link rel="stylesheet" href="{{ public_path('css/materialicons.css') }}" media="all" /> --}}
     <link rel="stylesheet" href="{{ public_path('css/wkhtml.css') }}" media="all" />
 </head>
 <body style="border:none">
